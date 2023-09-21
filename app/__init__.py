fba0550df114274aae5e4620788aaa3a11cb24ae
Joaquin Lamelas <joaquinlamelas@gmail.com>
2023-09-21 13:48:36 -0300

    Listo para hacer Despliegue

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config.from_mapping(
-        SENDGRID_KEY=os.environ.get('SENDGRID_KEY'),
+        DATABASE_PORT=os.environ.get('FLASK_DATABASE_PORT'),
     )
 
     from . import portfolio
